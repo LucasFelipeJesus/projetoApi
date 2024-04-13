3f60f0bf87d22dd5ddfe5a01dd98cd1915d42312
LucasFelipeJesus <lucasfelipejesus@gmail.com>
2024-04-13 09:39:24 -0300

    Criação de BD Mysql e SQL

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -5,17 +5,58 @@ const database = new DataSource({
     type: 'sqlite',
     database: './src/database/database.sqlite',
     logging: true,
-    synchronize: false,
+    synchronize: true,
     entities:
         [
             join(__dirname, '..', 'models/*.{ts,js}')
         ]
 })
+/*
+const db2DataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "",
+    database: "api",
+    entities: [__dirname + "/entity/*{.js,.ts}"],
+    synchronize: true,
+})
+
+const db3DataSource = new DataSource({
+    type: "mssql",
+    host: "localhost",
+    port: 1433,
+    username: "sa",
+    password: "Caslueisla!@#",
+    database: "Api",
+    options: {
+        encrypt: false,
+
+    },
+    entities: [__dirname + "/entity/*{.js,.ts}"],
+    synchronize: true,
+
+})
 
 database.initialize()
     .then(() => {
-        console.log('Banco de Dados inciciado!')
+        console.log('Banco Sqlite de Dados inciciado!')
     }).catch(() => {
-        console.log('Falha!!! Banco de Dados não inciciado!')
+        console.log('Falha!!! Banco Sqlite de Dados não inciciado!')
     })
-export default database
+
+db2DataSource.initialize()
+    .then(() => {
+        console.log('Banco Mysql de Dados inciciado!')
+    }).catch(() => {
+        console.log('Falha!!! Banco Mysql de Dados não inciciado!')
+    })
+
+db3DataSource.initialize()
+    .then(() => {
+        console.log('Banco SqlServer de Dados inciciado!')
+    }).catch(() => {
+        console.log('Falha!!! Banco Sqlserver de Dados não inciciado!')
+    })*/
+export default database;// db2DataSource; db3DataSource
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express  from 'express';
+import express from 'express';
 import database from './database/ormconfig'
+//import db2DataSource from './database/ormconfig'
+//import db3DataSource from './database/ormconfig'
 import routes from './routes';
 import cors from 'cors';
 
@@ -11,7 +13,12 @@ app.use(routes)
 app.use(cors())
 
 app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}`);
-    console.log(database.isInitialized ? 'Banco Dados OK! :':'Banco Carregando!')
-  })
+  console.log(`Servidor executando na porta ${port}`);
+
+  console.log(database.isInitialized ? 'Banco Sqllite Dados OK! :' : 'Banco Sqllite Carregando!')
+
+  //console.log(db2DataSource.isInitialized ? 'Banco Mysql Dados OK! :' : 'Banco Mysql Carregando!')
+
+  //console.log(db3DataSource.isInitialized ? 'Banco Sqlserver Dados OK! :' : 'Banco Sqlserver Carregando!')
+})
 
